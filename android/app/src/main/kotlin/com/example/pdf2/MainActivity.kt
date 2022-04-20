d4e6edce6fc6c31d5f15bb2c12d20c0aa1f0482b
Robin Hood <yoga3911@gmail.com>
2022-04-20 20:58:25 +0700

    init

---
+++ android/app/src/main/kotlin/com/example/pdf2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pdf2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'page/pdf_page.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static String title = 'Invoice';
+
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: title,
+        theme: ThemeData(primarySwatch: Colors.deepOrange),
+        home: const PdfPage(),
+      );
+}
